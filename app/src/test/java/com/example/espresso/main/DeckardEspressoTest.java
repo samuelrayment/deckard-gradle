42e7bc0a7de7b76e24a29a63596472fe0203c77a
Samuel Rayment <sam.rayment@thisisglobal.com>
2015-01-19 15:36:57 +0000

    Migrated to espresso 2.0

--- app/src/test/java/com/example/espresso/main/DeckardEspressoTest.java
+++ app/src/test/java/com/example/espresso/main/DeckardEspressoTest.java
@@ -5,11 +5,12 @@ import android.test.suitebuilder.annotation.LargeTest;
 import com.example.R;
 import com.example.main.MainActivity;
 
-import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
-import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
-import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 
 @LargeTest
 public class DeckardEspressoTest extends ActivityInstrumentationTestCase2<MainActivity> {
@@ -31,6 +32,8 @@ public class DeckardEspressoTest extends ActivityInstrumentationTestCase2<MainAc
         onView(withId(R.id.text))
             .check(matches(withText("Hello Espresso!")));
     }
+
+
 
     public void testCheckButtonUpdatesLabel() {
         onView(withId(R.id.button))
--- app/src/test/java/com/example/utils/MultiDexTestRunner.java
+++ app/src/test/java/com/example/utils/MultiDexTestRunner.java
@@ -2,13 +2,12 @@ package com.example.utils;
 
 import android.os.Bundle;
 import android.support.multidex.MultiDex;
-
-import com.google.android.apps.common.testing.testrunner.GoogleInstrumentationTestRunner;
+import android.support.test.runner.AndroidJUnitRunner;
 
 /**
  * Espresso test runner that also supports multidex
  */
-public class MultiDexTestRunner extends GoogleInstrumentationTestRunner {
+public class MultiDexTestRunner extends AndroidJUnitRunner {
     @Override
     public void onCreate(Bundle arguments) {
         MultiDex.install(getTargetContext());
